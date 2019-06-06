a222ecd2e0902e6291ca501fc04aab69b97feda7
Amit Khanna <amit.khanna@inmobi.com>
2019-06-06 12:35:54 +0530

    maa1 removal

--- common/src/main/java/org/apache/falcon/ColoMigration.java
+++ common/src/main/java/org/apache/falcon/ColoMigration.java
@@ -7,16 +7,21 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import javax.xml.bind.JAXBException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.falcon.entity.parser.EntityParser;
 import org.apache.falcon.entity.parser.EntityParserFactory;
 import org.apache.falcon.entity.v0.EntityType;
+import org.apache.falcon.entity.v0.feed.Cluster;
 import org.apache.falcon.entity.v0.feed.Feed;
+import org.apache.falcon.entity.v0.feed.Location;
+import org.apache.falcon.entity.v0.feed.Validity;
 import org.apache.falcon.entity.v0.process.Process;
 import org.apache.hadoop.fs.Path;
 
@@ -30,7 +35,7 @@ public class ColoMigration {
         }
         String entitytype = args[0].trim().toLowerCase();
         String oldEntities = args[1];
-        String outpath = args[2];`
+        String outpath = args[2];
         changeEntities(entitytype, oldEntities, outpath);
     }
 
@@ -42,6 +47,9 @@ public class ColoMigration {
         System.out.println("Number of files: " + listOfFiles.length);
         for (File file : listOfFiles) {
             if (file.isFile()) {
+                if (file.getName().contains("merlin")) {
+                    continue;
+                }
                 System.out.println(file.getName());
                 EntityType type = EntityType.getEnum(entityType);
                 EntityParser<?> entityParser = EntityParserFactory.getParser(type);
@@ -55,104 +63,168 @@ public class ColoMigration {
                             org.apache.falcon.entity.v0.process.Clusters entityClusters = process.getClusters();
                             List<org.apache.falcon.entity.v0.process.Cluster> clusters = entityClusters.getClusters();
 
+
                             List<org.apache.falcon.entity.v0.process.Cluster> processClusterToRemove = new ArrayList<>();
                             for (org.apache.falcon.entity.v0.process.Cluster cluster : clusters) {
-                                if (cluster.getName().equals("hkg1-opal") || cluster.getName().equals("uj1-topaz") || cluster.getName().equals("wc1-ssp")
-                                        || cluster.getName().equals("uh1-krypton")) {
+                                if (cluster.getName().equals("maa1-beryl")) {
                                     processClusterToRemove.add(cluster);
                                 }
                             }
                             clusters.removeAll(processClusterToRemove);
 
-                            // filter on start date for processes
+//                            org.apache.falcon.entity.v0.process.Cluster clusterToadd = new org.apache.falcon.entity.v0.process.Cluster();
+//
+//
+//                            boolean filterAmsP =false;
+//
+//                            // filter on start date for processes
                             boolean filter = false;
                             List<String> processClusterNames = new ArrayList<>();
                             for (org.apache.falcon.entity.v0.process.Cluster cluster : clusters) {
                                 Date clusterDate = cluster.getValidity().getEnd();
                                 processClusterNames.add(cluster.getName());
-                                if (clusterDate.getTime() > System.currentTimeMillis() ) {
+                                if (clusterDate.getTime() > System.currentTimeMillis()) {
                                     filter = true;
                                 }
+//                                if(filterAmsP == false && cluster.getName().equals("ams1-azurite")){
+//                                    clusterToadd.setName("maa1-beryl");
+//                                    clusterToadd.setSla(cluster.getSla());
+//                                    org.apache.falcon.entity.v0.process.Validity validity = new org.apache.falcon.
+//                                            entity.v0.process.Validity();
+//                                    validity.setStart(cluster.getValidity().getStart());
+//                                    validity.setEnd(cluster.getValidity().getEnd());
+//                                    clusterToadd.setValidity(validity);
+//                                    clusterToadd.setVersion(cluster.getVersion());
+//                                    filterAmsP = true;
+//                                }
                             }
-
-                            if(processClusterNames.size() != 0) {
-                                processClusterNames.add("prism");
-                            }
-
-                            if (filter) {
-                                for (String colo : processClusterNames) {
-                                    File entityFile = new File(new Path(newPath + File.separator + colo + File.separator +
-                                            file.getName()).toUri().toURL().getPath());
-                                    entityFile.getParentFile().mkdirs();
-                                    System.out.println("File path : " + entityFile.getAbsolutePath());
-                                    if (!entityFile.createNewFile()) {
-                                        System.out.println("Not able to stage the entities in the tmp path");
-                                        return;
+//
+                                if (processClusterNames.size() != 0) {
+                                    processClusterNames.add("prism");
+                                }
+//
+//
+//                            if(filterAmsP == true) {
+//                                clusterToadd.setName("maa1-beryl");
+//                                clusterToadd.getValidity().setStart(new Date(1542240000000L));
+//                                clusters.add(clusterToadd);
+//                                processClusterNames.add(clusterToadd.getName());
+//                            }
+//
+                                if (filter) {
+                                    for (String colo : processClusterNames) {
+                                        File entityFile = new File(newPath + File.separator + colo + File.separator +
+                                                file.getName());
+                                        entityFile.getParentFile().mkdirs();
+                                        if (!entityFile.createNewFile()) {
+                                            System.out.println("Not able to stage the entities in the tmp path");
+                                            return;
+                                        }
+                                        out = new FileOutputStream(entityFile);
+                                        type.getMarshaller().marshal(process, out);
+                                        out.close();
                                     }
-                                    out = new FileOutputStream(entityFile);
-                                    type.getMarshaller().marshal(process, out);
-                                    out.close();
-                                }
-                            }
-
-                            break;
-
-
-                        case FEED:
-                            Feed feed = (Feed) entityParser.parse(xmlStream);
-
-                            org.apache.falcon.entity.v0.feed.Clusters feedClusters = feed.getClusters();
-                            List<org.apache.falcon.entity.v0.feed.Cluster> feed_clusters = feedClusters.getClusters();
-
-                            List<org.apache.falcon.entity.v0.feed.Cluster> feedClusterToRemove = new ArrayList<>();
-                            for (org.apache.falcon.entity.v0.feed.Cluster cluster : feed_clusters) {
-                                if (cluster.getName().equals("hkg1-opal") || cluster.getName().equals("uj1-topaz")
-                                        || cluster.getName().equals("wc1-ssp") || cluster.getName().equals("uh1-krypton")
-                                        || cluster.getName().equals("hkg1-opal-secondary")) {
-                                    feedClusterToRemove.add(cluster);
                                 }
 
-                                if(cluster.getName().equals("uh1-krypton-secondary")) {
-                                    cluster.setName("dfw1-onyx-secondary");
-                                    Date date = new Date(1501200000L);
-                                    cluster.getValidity().setStart(date);
-                                }
-                            }
-                            feed_clusters.removeAll(feedClusterToRemove);
+                                break;
 
-                            List<String> feedClusterNames = new ArrayList<>();
-                            for (org.apache.falcon.entity.v0.feed.Cluster cluster : feed_clusters) {
-                                feedClusterNames.add(cluster.getName());
+
+                                case FEED:
+                                    Feed feed = (Feed) entityParser.parse(xmlStream);
+
+                                    org.apache.falcon.entity.v0.feed.Clusters feedClusters = feed.getClusters();
+                                    List<org.apache.falcon.entity.v0.feed.Cluster> feed_clusters = feedClusters.getClusters();
+
+
+                                    List<org.apache.falcon.entity.v0.feed.Cluster> feedClusterToRemove = new ArrayList<>();
+                                    for (org.apache.falcon.entity.v0.feed.Cluster cluster : feed_clusters) {
+                                        if (cluster.getName().equals("maa1-beryl")) {
+                                            feedClusterToRemove.add(cluster);
+                                        }
+                                    }
+                                    feed_clusters.removeAll(feedClusterToRemove);
+
+
+                                    Cluster clusterToaddFeed = new Cluster();
+//
+//
+//                            boolean filterAms = false;
+                                    List<String> feedClusterNames = new ArrayList<>();
+                                    for (org.apache.falcon.entity.v0.feed.Cluster cluster : feed_clusters) {
+                                        feedClusterNames.add(cluster.getName());
+//                                if(cluster.getName().equals("maa1-beryl")){
+//                                    Validity validity = new Validity();
+//                                    validity.setStart(cluster.getValidity().getStart());
+//                                    validity.setEnd(cluster.getValidity().getEnd());
+//                                    clusterToaddFeed.setValidity(validity);
+//                                    clusterToaddFeed.setVersion(cluster.getVersion());
+//                                    clusterToaddFeed.setName(cluster.getName());
+//                                    clusterToaddFeed.setDelay(cluster.getDelay());
+//                                    clusterToaddFeed.setExport(cluster.getExport());
+//                                    clusterToaddFeed.setImport(cluster.getImport());
+//                                    clusterToaddFeed.setType(cluster.getType());
+//                                    clusterToaddFeed.setTable(cluster.getTable());
+//                                    clusterToaddFeed.setSla(cluster.getSla());
+//                                    clusterToaddFeed.setRetention(cluster.getRetention());
+//                                    clusterToaddFeed.setPartition(cluster.getPartition());
+//                                    clusterToaddFeed.setLocations(cluster.getLocations());
+//                                    clusterToaddFeed.setLifecycle(cluster.getLifecycle());
+
+//                                    if(cluster.getLocations()!=null) {
+//
+//                                        for (Location location : cluster.getLocations().getLocations()) {
+//                                            if (StringUtils.contains(location.getPath(), "ams1")) {
+//                                                location.setPath(location.getPath().replace("ams1", "maa1"));
+//                                                filterAms = true;
+//                                            } else if (StringUtils.contains(location.getPath(), "Ams1")) {
+//                                                location.setPath(location.getPath().replace("Ams1", "Maa1"));
+//                                                filterAms = true;
+//                                            } else if (StringUtils.contains(location.getPath(), "AMS1")) {
+//                                                location.setPath(location.getPath().replace("AMS1", "MAA1"));
+//                                                filterAms = true;
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//                            }
+                                        if (feedClusterNames.size() != 0) {
+                                            feedClusterNames.add("prism");
+                                        }
+
+//                            if(filterAms == true) {
+
+//                                clusterToaddFeed.setName("maa1-beryl");
+//
+//                                clusterToaddFeed.getValidity().setStart(new Date(1535760000000L));
+//                                feed_clusters.add(clusterToaddFeed);
+//                                feedClusterNames.add(clusterToaddFeed.getName());
+//                            }
+                                        for (String colo : feedClusterNames) {
+
+                                            File entityFile = new File(newPath + File.separator + colo + File.separator
+                                                    + file.getName());
+                                            entityFile.getParentFile().mkdirs();
+                                            System.out.println("File path : " + entityFile.getAbsolutePath());
+                                            if (!entityFile.createNewFile()) {
+                                                System.out.println("Not able to stage the entities in the tmp path");
+                                                return;
+                                            }
+                                            out = new FileOutputStream(entityFile);
+                                            type.getMarshaller().marshal(feed, out);
+                                            out.close();
+                                        }
+                                    }
                             }
-                            if(feedClusterNames.size() != 0) {
-                                feedClusterNames.add("prism");
-                            }
-                            for (String colo : feedClusterNames) {
-                                File entityFile = new File(new Path(newPath + File.separator + colo + File.separator
-                                        + file.getName()).toUri().toURL().getPath());
-                                entityFile.getParentFile().mkdirs();
-                                System.out.println("File path : " + entityFile.getAbsolutePath());
-                                if (!entityFile.createNewFile()) {
-                                    System.out.println("Not able to stage the entities in the tmp path");
-                                    return;
-                                }
-                                out = new FileOutputStream(entityFile);
-                                type.getMarshaller().marshal(feed, out);
-                                out.close();
-                            }
+                    } catch(FileNotFoundException e){
+                        System.out.println(e.toString());
+                    } catch(FalconException e){
+                        System.out.println(e.toString());
+                    } catch(IOException e){
+                        e.printStackTrace();
+                    } catch(Exception e){
+                        System.out.println(e.toString());
                     }
-                } catch (FileNotFoundException e) {
-                    System.out.println(e.toString());
-                } catch (FalconException e) {
-                    System.out.println(e.toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JAXBException e) {
-                    System.out.println(e.toString());
-                } catch (Exception e) {
-                    System.out.println(e.toString());
                 }
             }
         }
     }
-}
